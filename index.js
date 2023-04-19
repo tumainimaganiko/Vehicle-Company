0cb6765c6b8d59e0b15dfa33237bc2410296e41b
tumainimaganiko <tumainimaganiko@gmail.com>
2023-04-19 21:09:38 +0300

    Update Desktop View

--- index.js
+++ index.js
@@ -17,42 +17,42 @@ openMenu.addEventListener('click', () => {
 
 const investors = [
   {
-    img: 'images/forest engineer.jpeg',
+    img: 'images/reagan.png',
     heading: 'Frank Paul',
     info: 'Engineer in TESLA',
     details:
       'He supports us in importing and exporting Cars in any part of the world',
   },
   {
-    img: 'images/girl engineer.jpeg',
+    img: 'images/julia.png',
     heading: 'Agnes Barnabas',
     info: 'Designer at GOOGLE',
     details:
       'She supports us in importing and exporting Cars in any part of the world',
   },
   {
-    img: 'images/spectacles engineer.jpeg',
+    img: 'images/kilnam.png',
     heading: 'Peter Paul',
     info: 'Manager at BARCLAYS',
     details:
       'He supports us in importing and exporting Cars in any part of the world',
   },
   {
-    img: 'images/male engineer.jpeg',
+    img: 'images/lila.png',
     heading: 'Oyedepo Elia',
     info: 'Engineer at TOYOTA',
     details:
       'He supports us in importing and exporting Cars in any part of the world',
   },
   {
-    img: 'images/2maini.jpg',
+    img: 'images/yochai.png',
     heading: 'Tumaini Maganiko',
     info: 'Student at Microverse',
     details:
       'He supports us in importing and exporting Cars in any part of the world',
   },
   {
-    img: 'images/spectacles engineer.jpeg',
+    img: 'images/sohyeong.png',
     heading: 'John Barnabas',
     info: 'Engineer at TESLA',
     details:
@@ -66,7 +66,10 @@ investors.forEach((value) => {
   const div = document.createElement('div');
   div.className = 'd-flex align-items-center';
   div.innerHTML = `
-    <img src="${value.img}" alt="Investor">
+    <div class="x-shade">
+      <img src="${value.img}" alt="Investor">
+      <div class="inner-div"></div>
+    </div>
     <div class="content">
         <h3>${value.heading}</h3>
         <p class="info">${value.info}</p>
