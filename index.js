6390dafa3e50558c604270b27968557362bee0a0
tumainimaganiko <tumainimaganiko@gmail.com>
2023-04-19 13:51:21 +0300

    Modify Interaction

--- index.js
+++ index.js
@@ -19,31 +19,37 @@ const investors = [
     {
         img: "images/forest engineer.jpeg",
         heading: "Frank Paul",
+        info: "Engineer in TESLA",
         details: "He supports us in importing and exporting Cars in any part of the world"
     },
     {
         img: "images/girl engineer.jpeg",
         heading: "Agnes Barnabas",
+        info: "Designer at GOOGLE",
         details: "She supports us in importing and exporting Cars in any part of the world"
     },
     {
         img: "images/spectacles engineer.jpeg",
         heading: "Peter Paul",
+        info: "Manager at BARCLAYS",
         details: "He supports us in importing and exporting Cars in any part of the world"
     },
     {
         img: "images/male engineer.jpeg",
         heading: "Oyedepo Elia",
+        info: "Engineer at TOYOTA",
         details: "He supports us in importing and exporting Cars in any part of the world"
     },
     {
         img: "images/2maini.jpg",
         heading: "Tumaini Maganiko",
+        info: "Professor at HARVARD",
         details: "He supports us in importing and exporting Cars in any part of the world"
     },
     {
         img: "images/spectacles engineer.jpeg",
         heading: "John Barnabas",
+        info: "Engineer at TESLA",
         details: "He supports us in importing and exporting Cars in any part of the world"
     }
 ]
@@ -57,6 +63,7 @@ investors.forEach((value,index) => {
     <img src="${value.img}" alt="">
     <div class="content">
         <h3>${value.heading}</h3>
+        <p class="info">${value.info}</p>
         <p>${value.details}</p>
     </div>
     `
