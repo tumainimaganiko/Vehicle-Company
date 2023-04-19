471c704a730e0f2d9cdf5558ce903361114ca313
tumainimaganiko <tumainimaganiko@gmail.com>
2023-04-19 08:11:22 +0300

    Create Array

--- index.js
+++ index.js
@@ -14,3 +14,36 @@ openMenu.addEventListener('click', () => {
     mobileMenu.style.display = 'block';
     head.style.display = 'none';
 })
+
+const investors = [
+    {
+        img: "images/forest engineer.jpeg",
+        heading: "Frank Paul",
+        details: "He supports us in importing and exporting Cars in any part of the world"
+    },
+    {
+        img: "images/girl engineer.jpeg",
+        heading: "Agnes Barnabas",
+        details: "She supports us in importing and exporting Cars in any part of the world"
+    },
+    {
+        img: "images/spectacles engineer.jpeg",
+        heading: "Peter Paul",
+        details: "He supports us in importing and exporting Cars in any part of the world"
+    },
+    {
+        img: "images/male engineer.jpeg",
+        heading: "Oyedepo Elia",
+        details: "He supports us in importing and exporting Cars in any part of the world"
+    },
+    {
+        img: "images/2maini.jpeg",
+        heading: "Tumaini Maganiko",
+        details: "He supports us in importing and exporting Cars in any part of the world"
+    },
+    {
+        img: "images/spectacles engineer.jpeg",
+        heading: "John Barnabas",
+        details: "He supports us in importing and exporting Cars in any part of the world"
+    }
+]
